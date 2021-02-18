42218ae44e2c5d4250691b4aef64210631ec9db7
Никита Левашов <sh4rpy@ya.ru>
2021-02-18 22:38:38 +0300

    abandoned the search in favor of a tag filter & added new test case for filtering

--- processor/views.py
+++ processor/views.py
@@ -22,7 +22,7 @@ class TaskListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tags'] = Tag.objects.all()
-        context['index'] = True
+        context['index'] = True  # показываем фильтр по тегам только на главной
         return context
 
 
