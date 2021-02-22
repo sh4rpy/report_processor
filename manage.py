90cf7e6bef9ef1819311d5316f0841c0cbf6b39e
Никита Левашов <sh4rpy@ya.ru>
2021-02-23 00:09:39 +0300

    minor update

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative processor."""
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'report_processor.settings')
     try:
         from django.core.management import execute_from_command_line
